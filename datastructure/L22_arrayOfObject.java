16888ea206689a1c59067d12e1644a98a21e39e7
namitrathod <namitrathod07@gmail.com>
2024-01-03 20:14:41 +0530

    array

---
+++ datastructure/L22_arrayOfObject.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L22_arrayOfObject {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] str = new String[4];
+
+        for(int i=0; i< str.length; i++){
+            str [i]=in.next();
+        }
+        System.out.println(Arrays.toString(str));
+    }
+}
---
+++ datastructure/L24_2Darray_input.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L24_2Darray_input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int [][] arr = new int [3][3];
+        // arr.lenght give number of rows
+        
+        for(int row=0; row <arr.length; row++){
+            for(int col=0; col< arr[row].length; col++){
+                arr[row][col]= in.nextInt();
+            }
+        }
+
+//////////////////////Output of 2d array ///////////////////////////////////////////////////////
+        for(int row=0; row <arr.length; row++){
+            for(int col=0; col< arr[row].length; col++){
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }
+//       output of above code
+        // 41 42 43
+        // 44 45 46
+        // 47 48 49
+
+        for(int row =0; row < arr.length; row ++){
+            System.out.println(Arrays.toString(arr[row]));
+        }
+        /* output
+        [41, 42, 43]
+        [44, 45, 46]
+        [47, 48, 49]
+        */
+/////////////////////////////////// enhanced for loop ///////////////////////////
+        for(int[] a:arr){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
