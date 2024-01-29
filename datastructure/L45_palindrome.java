66d3734d97514b3dc16cf088a04f8c2a850078a4
namitrathod <namitrathod07@gmail.com>
2024-01-29 13:48:30 +0530

    string and palindrome

---
+++ datastructure/L45_palindrome.java
@@ -0,0 +1,20 @@
+public class L45_palindrome {
+    public static void main(String [] args){
+    String str = "abcdcba";
+    System.out.println("Is palindrome - "+isPalindrome(str));
+        
+    }
+    static boolean isPalindrome(String str){
+        str = str.toLowerCase();
+        for(int i=0; i<str.length()/2;i++){
+            char start =str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if(start == end){
+
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
